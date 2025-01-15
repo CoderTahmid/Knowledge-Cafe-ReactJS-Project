3521408b7e2c92d98a313c5b87b71f31d5617a3f
CoderTahmid <tahmid.ibne@gmail.com>
2025-01-15 10:09:13 +0600

    project done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,43 @@
 import PropTypes from 'prop-types';
+import { FaRegBookmark } from "react-icons/fa";
 
-const Blog = ({ blog }) => {
-    console.log(blog);
+const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
+    const { id, title, cover, reading_time, author_img, author, posted_date, hashtags } = blog;
     return (
-        <div>
-
+        <div className='mb-20 space-y-4'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className="flex justify-between mb-4">
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)} className='ml-2 text-red-600 text-2xl'><FaRegBookmark /></button>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
+            <button
+                className='text-purple-800 font-bold underline'
+                onClick={() => handleMarkAsRead(id, reading_time)}>
+                Mark as read
+            </button>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func.isRequired,
+    handleMarkAsRead: PropTypes.func.isRequired
 };
 
 export default Blog;
